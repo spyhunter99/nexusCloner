2719b2121a289759f116fa189d7d4f63e14c3103
Alex O'Ree <alexoree@apache.org>
2022-03-10 12:38:30 -0500

    NOJIRA fix for prompting for password even if it was passed in as an arg #3 retries for downloads for slower or intermittent connections #2 ignore ssl is now optional

--- src/main/java/org/miloss/nexuscloner/Main.java
+++ src/main/java/org/miloss/nexuscloner/Main.java
@@ -45,34 +45,34 @@ import org.jsoup.nodes.Element;
  * @author alex.oree
  */
 public class Main {
-
+    
     static class Container {
-
+        
         public Container() {
         }
-
+        
         String url;
         String destinationPath;
         int size = -1;
         Exception ex;
-
+        
         private Container(String link, String absolutePath) {
             url = link;
             destinationPath = absolutePath;
         }
     }
-
+    
     static class Wrapper {
-
+        
         public Wrapper() {
             data = new ArrayList<>();
         }
-
+        
         public ArrayList<Container> data;
     }
-
+    
     static class DownloadWorker implements Runnable {
-
+        
         @Override
         public void run() {
             while (running && !filesToDownload.isEmpty()) {
@@ -84,9 +84,9 @@ public class Main {
                             //skip it
                         } else {
                             System.out.println("[" + filesToDownload.size() + "] downloading " + remove.url + " to " + remove.destinationPath);
-
+                            
                             f.getParentFile().mkdirs();
-
+                            
                             CloseableHttpClient client = HttpClients.createDefault();
                             HttpGet get = new HttpGet(remove.url);
                             if (cookie != null) {
@@ -95,7 +95,7 @@ public class Main {
                             if (auth != null) {
                                 get.addHeader("Authorization", "Basic " + auth);
                             }
-
+                            
                             CloseableHttpResponse execute = client.execute(get);
                             HttpEntity entity = execute.getEntity();
                             System.out.println(execute.getStatusLine().getStatusCode() + ": " + execute.getStatusLine().getReasonPhrase());
@@ -136,40 +136,43 @@ public class Main {
             }
             System.out.println(Thread.currentThread().getId() + " thread terminated");
         }
-
+        
     }
-
+    
     static boolean running = true;
     static ArrayList<Container> failures = new ArrayList<>();
-
+    
     static String sourceUrl = "";
     static String auth = null;
     static String cookie = null;
     static ConcurrentLinkedQueue<Container> filesToDownload = new ConcurrentLinkedQueue<>();
-
+    
     public static void main(String[] args) throws Exception {
-
+        
         final long now = System.currentTimeMillis();
         Options opts = new Options();
         opts.addOption("url", true, "root url of the repository you want to clone");
-
+        
         opts.addOption("delay", true, "wait time between downloads (throttling)");
         opts.addOption("index", false, "build the index, must run first");
         opts.addOption("download", false, "download");
         opts.addOption("cookie", false, "prompt for cookie token");
-        opts.addOption("username", true, "");
-        opts.addOption("password", true, "prompt for password");
+        opts.addOption("username", true, "username");
+        opts.addOption("password", true, "if absent, will you will be prompted for the password");
         opts.addOption("output", true, "output folder, default is ./output/");
         opts.addOption("threads", true, "threads for concurrent downloads");
         opts.addOption("help", false, "help");
-
+        opts.addOption("ignoreSSL", false, "if present, all SSL errors are ignored");
+        
         CommandLineParser parser = new DefaultParser();
         CommandLine parse = parser.parse(opts, args);
-
-        initSsl();
-        if (parse.hasOption("help") 
-                || !parse.hasOption("url") 
-                || (!parse.hasOption("index") && !parse.hasOption("download") )){
+        
+        if (parse.hasOption("ignoreSSL")) {
+            initSsl();
+        }
+        if (parse.hasOption("help")
+                || !parse.hasOption("url")
+                || (!parse.hasOption("index") && !parse.hasOption("download"))) {
             HelpFormatter f = new HelpFormatter();
             f.printHelp("java -jar nexusCloner-{VERSION}-jar-with-dependencies.jar {options}", opts);
             return;
@@ -179,17 +182,23 @@ public class Main {
         if (!url.endsWith("/")) {
             url = url + "/";
         }
-        if (parse.hasOption("username") && parse.hasOption("password")) {
+        if (parse.hasOption("username") && !parse.hasOption("password")) {
             System.out.println("Password: ");
-
+            
             String password = new String(System.console().readPassword());
+            auth = new String(Base64.encodeBase64(
+                    (parse.getOptionValue("username") + ":" + password).getBytes()
+            ));
+        }
+        if (parse.hasOption("username") && parse.hasOption("password")) {
+            String password = parse.getOptionValue("password");
             auth = new String(Base64.encodeBase64(
                     (parse.getOptionValue("username") + ":" + password).getBytes()
             ));
         }
         if (parse.hasOption("cookie")) {
             System.out.println("Cookie: ");
-
+            
             String password = new String(System.console().readPassword());
             cookie = password;
             System.out.println("using cookie " + cookie);
@@ -197,9 +206,10 @@ public class Main {
         sourceUrl = url;
         File outputFolder;
         if (parse.hasOption("output")) {
-            outputFolder= new File(parse.getOptionValue("output"));
-        } else 
-            outputFolder= new File("./output");
+            outputFolder = new File(parse.getOptionValue("output"));
+        } else {
+            outputFolder = new File("./output");
+        }
         outputFolder.mkdirs();
         Kryo kryo = new Kryo();
         if (parse.hasOption("index")) {
@@ -210,15 +220,15 @@ public class Main {
             while (iterator.hasNext()) {
                 data.add(iterator.next());
             }
-
+            
             Wrapper d = new Wrapper();
             d.data = data;
             // ...
             Output output = new Output(new FileOutputStream("index.bin"));
-
+            
             kryo.writeObject(output, d);
             output.close();
-
+            
         }
         if (parse.hasOption("download")) {
             if (filesToDownload.isEmpty()) {
@@ -243,7 +253,7 @@ public class Main {
                 DownloadWorker dw = new DownloadWorker();
                 new Thread(dw).start();
             }
-
+            
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -258,82 +268,93 @@ public class Main {
             System.console().readLine();
             running = false;
         }
-
+        
         System.out.println("processed in " + (System.currentTimeMillis() - now) + "ms");
     }
-
+    
     private static boolean isDirectory(String link) {
         return (link.endsWith("/"));
     }
-
+    
     private static void process(String url, File outputFolder) throws Exception {
         outputFolder.mkdirs();
         System.out.println("processing directory " + url);
-        Document doc;
-        if (auth != null) {
-            doc = Jsoup.connect(url).header("Authorization", "Basic " + auth).get();
-        } else if (cookie != null) {
-            doc = Jsoup.connect(url).header("Cookie", cookie).get();
-        } else {
-            doc = Jsoup.connect(url).get();
-        }
-
-        for (Element e : doc.select("a")) {
-            String link = e.attr("href");
-            if (!ignore(link)) {
-                if (isDirectory(link)) {
-                    process(link,
-                            outputFolder);
+        int retries = 3;
+        while (retries > 0) {
+            try {
+                Document doc;
+                if (auth != null) {
+                    doc = Jsoup.connect(url).header("Authorization", "Basic " + auth).get();
+                } else if (cookie != null) {
+                    doc = Jsoup.connect(url).header("Cookie", cookie).get();
                 } else {
-                    //download the file
-                    URL urlFile = new URL(link);
-                    String relativePath = link.replace(sourceUrl, "");
-                    File outputFile = new File(outputFolder.getAbsolutePath() + File.separator + relativePath);
-
-                    filesToDownload.add(new Container(link, outputFile.getAbsolutePath()));
-                    //FileUtils.copyURLToFile(urlFile, outputFile );
+                    doc = Jsoup.connect(url).get();
                 }
-                //System.out.println(link);
+                
+                for (Element e : doc.select("a")) {
+                    String link = e.attr("href");
+                    if (!ignore(link)) {
+                        if (isDirectory(link)) {
+                            process(link,
+                                    outputFolder);
+                        } else {
+                            //download the file
+                            URL urlFile = new URL(link);
+                            String relativePath = link.replace(sourceUrl, "");
+                            File outputFile = new File(outputFolder.getAbsolutePath() + File.separator + relativePath);
+                            
+                            filesToDownload.add(new Container(link, outputFile.getAbsolutePath()));
+                            //FileUtils.copyURLToFile(urlFile, outputFile );
+                        }
+                        //System.out.println(link);
+                    }
+                }
+                return;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                retries--;
             }
         }
-
+        
+        throw new Exception("retry count exceeded");
+        
     }
-
+    
     private static boolean ignore(String link) {
         if (link.endsWith("../")) {
             return true;
         }
         return false;
     }
-
+    
     private List<String> getDirectoryListing(String nexusUrl) throws Exception {
         List<String> ret = new ArrayList<>();
-
+        
         Document doc = Jsoup.connect(nexusUrl).get();
         for (Element file : doc.select("td a")) {
             System.out.println(file.attr("href"));
             if (!file.attr("href").equalsIgnoreCase("../")) {
                 ret.add(file.attr("href"));
             }
-
+            
         }
-
+        
         return ret;
     }
-
+    
     private static void initSsl() throws Exception {
         TrustManager[] trustall = new TrustManager[]{new X509TrustManager() {
-
+            
             @Override
             public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
                 System.out.println("Trust no one");
             }
-
+            
             @Override
             public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
                 System.out.println("Trust no one");
             }
-
+            
             @Override
             public X509Certificate[] getAcceptedIssuers() {
                 System.out.println("Trust no one");
@@ -341,7 +362,7 @@ public class Main {
             }
         }};
         SSLContext sc = SSLContext.getInstance("SSL");
-
+        
         sc.init(null, trustall, new SecureRandom());
         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
